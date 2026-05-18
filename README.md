5054275ad213d679398b17b5a64d2aa3d44f33f4
Christian Villa <christian@gmail.com>
2026-05-17 22:02:12 -0500

    Prctica para la prueba

--- README.md
+++ README.md
@@ -1,18 +1,52 @@
-## Getting Started
+## *Practica Prueba de metodos de ordenaiento*
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+## *Objetivo*
 
-## Folder Structure
+Implementar los métodos sortByTitle(Movie[] movies) y sortByYear(Movie[] movies)
+en la carpeta controllers, para ordenar películas por título y por año,
+en orden ascendente y descendente, utilizando los siguientes métodos
+de ordenamiento:
 
-The workspace contains two folders by default, where:
+- Burbuja Avanzado
+- Insertion Sort
+- Selection Sort
 
-- `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
+## *Que se debe editar* ? 
 
-Meanwhile, the compiled output files will be generated in the `bin` folder by default.
+1.Crea y edita solo en `src /  controllers  /MovieControllerBurbuja.java   / MovieControllerInsertion.java / MovieControllerSelection.java`
 
-> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
+2.Implementa los nombres de metodos `sortByTlitle - - sortByYear`  puede haber cambios para los nombres en cada controlador
+3.Crea una clase modelo `Movie` con atributos `title` y `realeaseYear`
+4.Na mas panita 
 
-## Dependency Management
+### *En cada controlador debe haber su respecivo metodo para imprimir*
 
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+## Entrada de ejemplo; 
+1. Inception (2010)
+2. avatar (2009)
+3. The Matrix (1999)
+4. AVATAR (2022)
+5. Avatar (2016)
+
+## Salida ordenada esperada:
+1. avatar (2009)
+2. AVATAR (2022)
+3. Avatar (2016)
+4. Inception (2010)
+5. The Matrix (1999)
+
+## Otro ejemplo:
+
+ANTES
+1. dune (2021)
+2. Dune (1984)
+3. DUNE (2024)
+4. Batman (2022)
+5. avatar (2009)
+
+DESPUÉS
+1. avatar (2009)
+2. Batman (2022)
+3. dune (2021)
+4. Dune (1984)
+5. DUNE (2024)
--- src/App.java
+++ src/App.java
@@ -1,5 +1,67 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("=== Evaluación: Ordenamiento de Películas por Título ===\n");
+        // imirpimir la lista de películas sin ordenar
+        // ordenar la lista de películas por título  y año de todos los metoso utilizando el método sortByTitle y sortByYear del
+        // MovieControllerBurbuja /MovieControllerInsertion / MovieControllerSelection 
+        // imprimir la lista de películas ordenada
+
+    }
+
+    public static Car[] generateCarsList() {
+        Movie[] cars = new Movie[50];
+        movies[0] = new Movie("Inception", 2010);
+        movies[1] = new Movie("avatar", 2009);
+        movies[2] = new Movie("The Dark Knight", 2008);
+        movies[3] = new Movie("Interstellar", 2014);
+        movies[4] = new Movie("AVATAR", 2022);
+        movies[5] = new Movie("Avatar", 2016);
+        movies[6] = new Movie("The Matrix", 1999);
+        movies[7] = new Movie("dune", 2021);
+        movies[8] = new Movie("Dune", 1984);
+        movies[9] = new Movie("DUNE", 2024);
+        movies[10] = new Movie("The Godfather", 1972);
+        movies[11] = new Movie("the matrix", 2020);
+        movies[12] = new Movie("THE MATRIX", 1998);
+        movies[13] = new Movie("Pulp Fiction", 1994);
+        movies[14] = new Movie("pulp fiction", 1993);
+        movies[15] = new Movie("Forrest Gump", 1994);
+        movies[16] = new Movie("Gladiator", 2000);
+        movies[17] = new Movie("The Shawshank Redemption", 1994);
+        movies[18] = new Movie("the shawshank redemption", 1995);
+        movies[19] = new Movie("Titanic", 1997);
+        movies[20] = new Movie("TITANIC", 1998);
+        movies[21] = new Movie("Jurassic Park", 1993);
+        movies[22] = new Movie("The Avengers", 2012);
+        movies[23] = new Movie("the avengers", 2013);
+        movies[24] = new Movie("Iron Man", 2008);
+        movies[25] = new Movie("Spider Man", 2002);
+        movies[26] = new Movie("SPIDER MAN", 2021);
+        movies[27] = new Movie("spider man", 2019);
+        movies[28] = new Movie("Batman Begins", 2005);
+        movies[29] = new Movie("BATMAN BEGINS", 2006);
+        movies[30] = new Movie("Doctor Strange", 2016);
+        movies[31] = new Movie("doctor strange", 2022);
+        movies[32] = new Movie("Black Panther", 2018);
+        movies[33] = new Movie("BLACK PANTHER", 2020);
+        movies[34] = new Movie("Thor Ragnarok", 2017);
+        movies[35] = new Movie("thor ragnarok", 2018);
+        movies[36] = new Movie("Captain America", 2011);
+        movies[37] = new Movie("CAPTAIN AMERICA", 2014);
+        movies[38] = new Movie("Hulk", 2003);
+        movies[39] = new Movie("HULK", 2008);
+        movies[40] = new Movie("Deadpool", 2016);
+        movies[41] = new Movie("DEADPOOL", 2018);
+        movies[42] = new Movie("Logan", 2017);
+        movies[43] = new Movie("Joker", 2019);
+        movies[44] = new Movie("JOKER", 2024);
+        movies[45] = new Movie("Shrek", 2001);
+        movies[46] = new Movie("SHREK", 2004);
+        movies[47] = new Movie("Cars", 2006);
+        movies[48] = new Movie("CARS", 2011);
+        movies[49] = new Movie("Finding Nemo", 2003);
+
+        return movies;
+
     }
 }
